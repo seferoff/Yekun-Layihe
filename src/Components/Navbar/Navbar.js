e9f76b0c75799ea568439306e228969a40ba3e75
seferoff <seferoff007@gmail.com>
2023-01-31 14:00:32 +0400

    ready

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { FiMenu } from "react-icons/fi";
 import { SlBasket } from "react-icons/sl";
 import "./Navbar.css";
 
@@ -9,36 +8,33 @@ const Navbar = () => {
   let classname = "clicked";
   const basket = useSelector((state) => state.basket);
 
-  const changeDisplay = () => {
-    
-  };
-
   return (
     <div className="navbar">
-      <div className="head-nav" id="nav">
+      <div className="head-nav">
         <div className="links">
-          <button className="menu" onClick={changeDisplay}>
-            <FiMenu />
-          </button>
           <NavLink
+            name="nav"
             className={({ isActive }) => (isActive ? classname : "link")}
             to="/"
           >
             Home
           </NavLink>
           <NavLink
+            name="nav"
             className={({ isActive }) => (isActive ? classname : "link")}
             to="/about"
           >
             About Us
           </NavLink>
           <NavLink
+            name="nav"
             className={({ isActive }) => (isActive ? classname : "link")}
             to="/products"
           >
             Products
           </NavLink>
           <a
+            name="nav"
             className="link"
             href="https://www.google.com/maps/place/251+Mustafa+Kamal+Atat%C3%BCrk,+G%C9%99nc%C9%99,+Az%C9%99rbaycan/@40.6812556,46.3574035,17z/data=!3m1!4b1!4m5!3m4!1s0x403f510d7845b7bd:0xc3c8cd1ae2673948!8m2!3d40.6812516!4d46.3595923"
             target="_blank"
@@ -47,6 +43,7 @@ const Navbar = () => {
             Locate Us
           </a>
           <NavLink
+            name="nav"
             className={({ isActive }) => (isActive ? classname : "link")}
             to="/contact"
           >
